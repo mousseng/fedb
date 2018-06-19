47173442ba26876415bc4d0593d04743d24cc0dc
mousseng <assaeva@gmail.com>
2018-06-18 21:20:39 -0400

    Add remaining skill tables
    I'm really not sure about these... modeling the restrictions could be
    done in a handful of ways, but I don't know what the most _usable_ of
    those ways would be. I've tentatively listed out all the move types,
    weapon types, and color types as bitfields (with the notable exception
    of "colored bow", since apparently that's a relevant thing).
    
    I could opt to make a Restrictions table and that lets me use a single
    fkey for this, but that seems really useless for all but the "query by
    hand" use-case.
    
    I could opt to make separate mapping tables to model the restrictions.
    This is a bit problematic, since we're spreading out these related items
    (move type, weapon type, skill type) across multiple tables, and I'm not
    keen on having nullable, mutually-exclusive fkeys for a single row.
    
    I could enumerate the color-weapon pairs and drop the color-specific
    bits. This may be the most appealing solution to me, I'm afraid.
    
    I'll think on it some more.

---
+++ fedb/Tables/Special.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [Heroes].[Special]
+(
+    [id] INT NOT NULL PRIMARY KEY,
+    [name] VARCHAR(50) NOT NULL,
+    [effect] VARCHAR(300) NOT NULL,
+    [cost] INT NOT NULL,
+    [cooldown] INT NOT NULL,
+
+    [is_exclusive] BIT NOT NULL,
+
+    [allow_infantry] BIT NOT NULL,
+    [allow_cavalry] BIT NOT NULL,
+    [allow_flier] BIT NOT NULL,
+    [allow_armor] BIT NOT NULL,
+
+    [allow_sword] BIT NOT NULL,
+    [allow_lance] BIT NOT NULL,
+    [allow_axe] BIT NOT NULL,
+    [allow_breath] BIT NOT NULL,
+    [allow_tome] BIT NOT NULL,
+    [allow_bow] BIT NOT NULL,
+    [allow_colored_bow] BIT NOT NULL,
+    [allow_dagger] BIT NOT NULL,
+    [allow_staff] BIT NOT NULL,
+
+    [allow_red] BIT NOT NULL,
+    [allow_blue] BIT NOT NULL,
+    [allow_green] BIT NOT NULL,
+    [allow_colorless] BIT NOT NULL
+)
